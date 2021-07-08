8469cfd796c919ef8022b9799f182d783341bda0
s52047qwas <578043031@qq.com>
2021-07-08 10:44:53 +0800

    Initial commit

--- test_case/conftest.py
+++ test_case/conftest.py
@@ -1,3 +1,11 @@
+import os
+import sys
+rootpath=str('/var/jenkins_home/workspace/py_auto39')
+syspath=sys.path
+sys.path=[]
+sys.path.append(rootpath)#将工程根目录加入到python搜索路径中
+sys.path.extend([rootpath+i for i in os.listdir(rootpath) if i[0]!="."])#将工程目录下的一级目录添加到python搜索路径中
+sys.path.extend(syspath)
 import pytest
 from tools.handle_request import SendRequest
 from tools.handle_phone import HandlePhone
